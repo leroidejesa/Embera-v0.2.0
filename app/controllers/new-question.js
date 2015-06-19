ca6441d168f60e74ebdd3c51e808ac50cc00ce75
leroi <leroidejesa@gmail.com>
2015-06-19 15:55:08 -0700

    add question date

--- app/controllers/new-question.js
+++ app/controllers/new-question.js
@@ -21,10 +21,12 @@ import Ember from "ember";
 export default Ember.Controller.extend({
   actions: {
     save: function() {
+      var newDate = new Date();
       var newQuestion = this.store.createRecord('question', {
         questionTitle: this.get('question-title'),
         author: this.get('author'),
-        description: this.get('description')
+        description: this.get('description'),
+        date: newDate
       });
       newQuestion.save();
       this.transitionToRoute('questions');
--- app/models/question.js
+++ app/models/question.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   questionTitle: DS.attr('string'),
   description: DS.attr('string'),
+  date: DS.attr('date'),
   author: DS.attr('string'),
   answers: DS.hasMany('answer', {async: true})
 });
