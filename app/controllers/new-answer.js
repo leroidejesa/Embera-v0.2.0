932c939f6518e2298fe09ac9f828ebb3ab2269ba
leroi <leroidejesa@gmail.com>
2015-06-19 14:22:25 -0700

    fix 'new answer' bugs

--- app/controllers/new-answer.js
+++ app/controllers/new-answer.js
@@ -1,16 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  needs: ['question'],
   actions: {
     save: function() {
-      var answer = this.get('model');
-      answer.save();
-
-      var controller = this;
-      answer.get('question').then(function(question) {
-        question.save();
-        controller.transitionToRoute('question', question);
+      var answer = this.store.createRecord('answer', {
+        text: this.get('text')
       });
+
+      var question = this.get('controllers.question.model');
+      answer.save().then(function() {
+        question.get('answers').pushObject(answer);
+        question.save();
+      });
+
+      this.transitionToRoute('question', question.id);
     }
   }
 });
