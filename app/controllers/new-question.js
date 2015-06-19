92a1e420aa85b4d70ff1daf64d0dbe9a41364c97
leroi <leroidejesa@gmail.com>
2015-06-19 16:15:34 -0700

    add styling for each question

--- app/controllers/new-question.js
+++ app/controllers/new-question.js
@@ -1,21 +1,3 @@
-// import Ember from 'ember';
-//
-// export default Ember.Controller.extend({
-//   actions: {
-//     save: function() {
-//       var newQuestion = this.store.createRecord('question', {
-//         questionTitle: this.get('question-title'),
-//         author: this.get('author'),
-//         description: this.get('description')
-//       });
-//       newQuestion.save();
-//       this.transitionToRoute('questions');
-//     }
-//   }
-// });
-
-
-
 import Ember from "ember";
 
 export default Ember.Controller.extend({
@@ -33,23 +15,3 @@ export default Ember.Controller.extend({
     }
   }
 });
-
-
-//
-// import Ember from "ember";
-//
-// var NewQuestionController = {
-//   actions: {
-//     save: function() {
-//       var newQuestion = this.store.createRecord('question', {
-//         questionTitle: this.get('question-title'),
-//         author: this.get('author'),
-//         description: this.get('description')
-//       });
-//       newQuestion.save();
-//       this.transitionToRoute('questions');
-//     }
-//   }
-// };
-//
-// export default Ember.ObjectController.extend(NewQuestionController);
--- app/controllers/question.js
+++ app/controllers/question.js
@@ -1,23 +1,3 @@
-// import Ember from 'ember';
-//
-// export default Ember.Controller.extend({
-//   actions: {
-//     edit: function() {
-//       this.set('isEditing', true);
-//     },
-//     save: function() {
-//       this.set('isEditing', false);
-//     },
-//     delete: function() {
-//       if (confirm('Are you sure?')) {
-//         this.get('model').destroyRecord();
-//       }
-//       this.transitionToRoute('questions');
-//     }
-//   }
-// });
-
-
 import Ember from "ember";
 
 var QuestionController = {
